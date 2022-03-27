af2c9ef54f1d8dd7ec8a61328a85b13e1fece4fc
pmallige-dev <72284504+pmallige-dev@users.noreply.github.com>
2022-03-27 12:01:49 +0530

    Cart Icon and Dropdown

--- src/routes/naviagtion/navigation.component.jsx
+++ src/routes/naviagtion/navigation.component.jsx
@@ -1,6 +1,9 @@
 import { Fragment, useContext } from "react";
 import { Link, Outlet } from "react-router-dom";
 
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import { UserContext } from "../../contexts/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
@@ -29,7 +32,9 @@ const Navigation = () => {
                             </Link>
                         )
                     }
+                    <CartIcon />
                 </div>
+                <CartDropdown />
             </div>
             <Outlet />
         </Fragment>
