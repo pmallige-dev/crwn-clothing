c235eb0b46d6a75e56bf0308421ecaa983534195
pmallige-dev <72284504+pmallige-dev@users.noreply.github.com>
2022-03-27 20:38:28 +0530

    Add to Cart Part 1

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,12 +1,29 @@
 import { createContext, useState } from "react";
 
+const addCartItem = (cartItems, productToAdd) => {
+    //find if cartItems contains products to add
+
+    //if found increment quantity
+
+    //return new array with modified cart items/ new cart items
+    
+}
+
 export const CartContext = createContext({
     isCartOpen: false,
     setIsCartOpen: () => {},
+    cartItems: [],
+    addItemToCart: () => {},
 });
 
 export const CartProvider = ({ children }) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
+    const [cartItems, setCartItems] = useState([]);
+
+    const addItemToCart = (productToAdd) => {
+        setCartItems(addCartItem(cartItems, productToAdd))
+    }
+
     const value = { isCartOpen, setIsCartOpen };
 
     const cartOnClickHandler = () => {
