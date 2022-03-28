6596985c5f3e007954c3eb3a2530563039c02102
pmallige-dev <72284504+pmallige-dev@users.noreply.github.com>
2022-03-28 09:22:18 +0530

    Creating Checkout page and adding increment functionality

---
+++ src/routes/checkout/checkout.component.jsx
@@ -0,0 +1,38 @@
+import { useContext } from 'react';
+
+import { CartContext } from '../../contexts/cart.context';
+
+import './checkout.styles.scss';
+
+const Checkout = () => {
+    const { cartItems, addItemToCart } = useContext(CartContext);
+
+    return (
+        <div className='checkout-container'>
+            <h1>Ich bin Checkouit Page</h1>
+            <div>
+                {
+                    cartItems.map((cartItem) => {
+                        const { id, name, quantity } = cartItem;
+                        return (
+                            <div key={id}>
+                                <h2>{name}</h2>
+                                <span>{quantity}</span>
+                                <br />
+                                <span>Decrement</span>
+                                <br />
+                                <span onClick={
+                                    () => addItemToCart(cartItem)}
+                                >
+                                    Increment
+                                </span>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Checkout;
