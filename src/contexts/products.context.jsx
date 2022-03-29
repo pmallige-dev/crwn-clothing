7fed52a39ca226dca511d36aa961d0aef5422fc9
pmallige-dev <72284504+pmallige-dev@users.noreply.github.com>
2022-03-29 18:23:19 +0530

    Get Products + Categories from Firestore

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,6 +1,6 @@
 import { createContext, useState, useEffect } from "react";
 
-import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils.js";
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils.js";
 
 //import SHOP_DATA from '../shopdata.js';
 
@@ -18,6 +18,14 @@ export const ProductsProvider = ({ children }) => {
     // useEffect(() => {
     //     addCollectionAndDocuments('categories', SHOP_DATA);
     // }, [])
+
+    useEffect(() => {
+        const getCategoriesMap = async () => {
+            const categoryMap = await getCategoriesAndDocuments();
+            console.log(categoryMap);
+        }
+        getCategoriesMap();   
+    }, [])
     const value = { products };
 
     return (<ProductsContext.Provider value={value}>{ children }</ProductsContext.Provider>)
