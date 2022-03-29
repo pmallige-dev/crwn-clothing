a491e4996c8e29bec5ba0a9a1effb387f18ed152
pmallige-dev <72284504+pmallige-dev@users.noreply.github.com>
2022-03-29 19:14:49 +0530

    Using our CategoriesMap

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -5,13 +5,13 @@ import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils.js";
 //import SHOP_DATA from '../shopdata.js';
 
 //As the actual value you want to access
-export const ProductsContext = createContext({
-    products: [],
+export const CategoriesContext = createContext({
+    categoriesMap: {},
 
 });
 
-export const ProductsProvider = ({ children }) => {
-    const [products, setProducts] = useState([]);
+export const CategoriesProvider = ({ children }) => {
+    const [categoriesMap, setCategoriesMap] = useState({});
 
     //This useEffect has to be called only once sinec this will add all the data into the DB.
     //We are calling this here in frontend since we dont have a backend
@@ -23,10 +23,11 @@ export const ProductsProvider = ({ children }) => {
         const getCategoriesMap = async () => {
             const categoryMap = await getCategoriesAndDocuments();
             console.log(categoryMap);
+            setCategoriesMap(categoryMap);
         }
         getCategoriesMap();   
     }, [])
-    const value = { products };
+    const value = { categoriesMap };
 
-    return (<ProductsContext.Provider value={value}>{ children }</ProductsContext.Provider>)
+    return (<CategoriesContext.Provider value={value}>{ children }</CategoriesContext.Provider>)
 }
