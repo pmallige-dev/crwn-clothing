d1112f33e20c0854bcf5ed969caf17466fbee212
pmallige-dev <72284504+pmallige-dev@users.noreply.github.com>
2022-03-31 16:24:55 +0530

    Cart Reducer Part 1

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, useReducer } from "react";
 
 const addCartItem = (cartItems, productToAdd) => {
     //find if cartItems contains products to add
@@ -51,6 +51,27 @@ export const CartContext = createContext({
     removeItemFromCart: () => { },
     clearItemFromCart: () => { },
 });
+
+const INITIAL_STATE = {
+    isCartOpen: false,
+    cartItems: [],
+    cartCount: 0,
+    cartTotal: 0,
+}
+
+const cartReducer = (state, action) => {
+    const { type, payload } = action;
+
+    switch (type) {
+        case 'SET_CART_ITEMS':
+            return {
+                ...state,
+                ...payload,
+            }
+        default:
+            throw new Error(`Unhandled type of ${type} in cartReducer`)
+    }
+}
 
 export const CartProvider = ({ children }) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
