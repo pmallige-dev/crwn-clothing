23365124f21aef4b7598284717173bb292447fae
pmallige-dev <72284504+pmallige-dev@users.noreply.github.com>
2022-04-10 21:46:48 +0530

    Redux-persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,11 @@
 import { compose, createStore, applyMiddleware } from "redux";
-// import logger from "redux-logger";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import { rootReducer } from "./root-reducer";
 
 const loggerMiddleware = (store) => (next) => (action) => {
-    if(!action.type){
+    if (!action.type) {
         return next(action);
     }
 
@@ -17,9 +18,23 @@ const loggerMiddleware = (store) => (next) => (action) => {
     console.log('nextState: ', store.getState());
 }
 
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 const middlewares = [loggerMiddleware];
 
 const composedEnhancers = compose(applyMiddleware(...middlewares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers)
+export const store = createStore(
+    persistedReducer,
+    undefined,
+    composedEnhancers
+);
+
+export const persistor = persistStore(store);
 
