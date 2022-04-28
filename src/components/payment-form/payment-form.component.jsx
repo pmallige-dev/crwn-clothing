9cdf528614d2664fbefa2d4668445ed2209c3403
pmallige-dev <72284504+pmallige-dev@users.noreply.github.com>
2022-04-28 21:22:47 +0530

    Finishing Stripe payment

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -11,14 +11,47 @@ const PaymentForm = () => {
     const paymentHandler = async (e) => {
         e.preventDefault();
 
-        // if(!stripe || !elements) {
-        //     return;
-        // }
+        if(!stripe || !elements) {
+            return;
+        }
+
+        const response = await fetch('.netlify/functions/create-payment-intent', {
+            method: 'post',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ amount: 10000 }),
+          }).then((res) => {
+            return res.json();
+          });
+
+          const {
+              paymentIntent: { client_secret }
+          } = response;
+
+          console.log(client_secret);
+
+          const paymentResult = await stripe.confirmCardPayment(client_secret, {
+              payment_method: {
+                  card: elements.getElement(CardElement),
+                  billing_details: {
+                      name: 'Prakyath Mallige'
+                  }
+              }
+          });
+
+          if(paymentResult.error) {
+              alert(paymentResult.error);
+          } else {
+              if(paymentResult.paymentIntent.status === 'succeeded') {
+                  alert('Payment Successful');
+              }
+          }
     }
 
     return (
         <PaymentFormContainer>
-            <FormContainer>
+            <FormContainer onSubmit={paymentHandler}>
                 <h2>Credit Card Payment: </h2>
                 <CardElement />
                 <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay Now</Button>
